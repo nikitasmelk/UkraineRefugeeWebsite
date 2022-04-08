c65fc5c46dcfa9c30d5d4394aed4e45fb8fd3e96
Nikita Smelkov <nikitasmelk@gmail.com>
2022-04-08 16:05:42 -0700

    General fix 22

--- script.js
+++ script.js
@@ -29,12 +29,6 @@ unwfp_img.style.backgroundImage = `url("images/unwfp.jpg")`;
 
 
 function ready(){
-
-  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-  let vh = window.innerHeight * 0.01;
-  // Then we set the value in the --vh custom property to the root of the document
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-
   for (let i = 0; i < document.querySelectorAll('.org_description_text').length; i++) {
     resizeText({
      element: document.querySelectorAll('.org_description_text')[i],
